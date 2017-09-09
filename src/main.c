d12d42bb669f7be0c22e380068f0b45ef959aebb
Unknown <nicolas.giannelos@epitech.eu>
2017-09-09 05:00:22 +0300

    minor modifications
    added sigaction
    fixed potential sigsegv with localtime()

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login SRJanel <n******.*********@epitech.eu>
 ** 
 ** Started on  Sat Aug 19 21:02:34 2017 
-** Last update Sat Sep  9 04:27:49 2017 
+** Last update Sat Sep  9 04:58:01 2017 
 */
 
 #include <time.h>
@@ -23,48 +23,59 @@
 #include "protocols.h"
 #include "utils.h"
 
+static volatile int	g_sd;
+
+static void		signal_handler(int signum)
+{
+  close(g_sd);
+  (void)signum;
+}
+
 inline static void	usage(const char * const prog_name)
 {
   fprintf(stderr, "USAGE: %s <iface>\n"					\
 	  "\t<iface>\t\tIf not specified, listening on all interfaces\n", prog_name);
 }
 
-static char		raw_bind_iface(const int sd, const char *iface)
+static char		raw_bind_iface(const char *iface)
 {
   struct ifreq		ifr;
   struct sockaddr_ll	sockaddr;
 
   memset(&ifr, 0, sizeof ifr);
   strncpy(ifr.ifr_name, iface, IFNAMSIZ - 1);
-  if (setsockopt(sd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof ifr) == -1
-      || ioctl(sd, SIOCGIFINDEX, &ifr) == -1)
+  if (setsockopt(g_sd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof ifr) == -1
+      || ioctl(g_sd, SIOCGIFINDEX, &ifr) == -1)
     return (-1);
   memset(&sockaddr, 0, sizeof(struct sockaddr_ll));
   sockaddr.sll_family = AF_PACKET;
   sockaddr.sll_ifindex = ifr.ifr_ifindex;
   sockaddr.sll_protocol = htons(ETH_P_ALL);
-  return (bind(sd, (struct sockaddr *)&sockaddr, sizeof sockaddr));
+  return (bind(g_sd, (struct sockaddr *)&sockaddr, sizeof sockaddr));
 }
 
 int			main(int argc, char *argv[])
 {
-  int			sd;
+  /* int			sd; */
   unsigned char		packet[IP_MAXPACKET] = {0};
   ssize_t		byte_read;
-  time_t		tm;
+  struct tm		*tm;
   char			tm_formatted[256];
-
+  struct sigaction	signal;
+  
+  signal.sa_handler = signal_handler;
+  sigaction(SIGINT, &signal, NULL);
   if (argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")))
     return (usage(argv[0]), EXIT_FAILURE);
-  if ((sd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1)
+  if ((g_sd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1)
     return (PRINT_ERROR("Socket creation failed:"), EXIT_FAILURE);
-  if (argc == 2 && raw_bind_iface(sd, argv[1]) == -1)
+  if (argc == 2 && raw_bind_iface(argv[1]) == -1)
     return (PRINT_ERROR("Bind failed:"), EXIT_FAILURE);
-  while ("true")
+  while ("it's on")
     {
-      tm = time(NULL);
-      strftime(tm_formatted, 256, "%B %d %Y-%T", localtime(&tm));
-      if ((byte_read = recvfrom(sd, packet, IP_MAXPACKET, 0, NULL, NULL)) <= 0)
+      if ((tm = localtime((time_t[1]){time(NULL)})) != (struct tm *)-1)
+	strftime(tm_formatted, 256, "%B %d %Y-%T", tm);
+      if ((byte_read = recvfrom(g_sd, packet, IP_MAXPACKET, 0, NULL, NULL)) <= 0)
 	return (PRINT_ERROR("Cannot capture:"), EXIT_FAILURE);
       fprintf(stdout, "\n\n******************* New Packet "	\
 	      "<%s> *******************\n", tm_formatted);
