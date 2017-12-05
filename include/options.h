46de8e09c782a2224aaddcaf3a2f77454936eb3f
Unknown <nicolas.giannelos@epitech.eu>
2017-12-05 00:43:34 +0000

    integrated x macros in options

--- include/options.h
+++ include/options.h
@@ -4,18 +4,33 @@
 ** Made by SRJanel
 ** Login SRJanel <n******.******s@epitech.eu>
 ** 
-** Started on  Mon Oct 23 01:27:28 2017 
-** Last update Mon Oct 23 01:33:31 2017 
+** Started on  Sat Oct 14 21:57:19 2017 
+** Last update Mon Dec  4 18:46:07 2017 
 */
 
 #ifndef OPTIONS_H_
 # define OPTIONS_H_
 
-struct	s_options {
-  int		help;
-  char		*interface;
-  int		promiscuous;
-  char		*filter;
-}		get_args(int argc, char **argv);
+# define STRINGIFY(x) #x
+# define MAKE_STRING(x) STRINGIFY(x)
+
+# define OPTIONS_WITH_ARG_TABLE						\
+  ENTRY(interface, required_argument, 'i')				\
+  ENTRY(filter, required_argument, 'f')					\
+
+# define OPTIONS_WITHOUT_ARG_TABLE		\
+  ENTRY(promiscuous, 'p')			\
+  ENTRY(help, 'h')
+
+# define OPTSTRING	"hpi:f:"
+
+extern struct s_options {
+# define ENTRY(FIELD, UNUSED1, UNUSED2) char *FIELD;
+  OPTIONS_WITH_ARG_TABLE
+# undef ENTRY
+# define ENTRY(FIELD, UNUSED1) int FIELD;
+  OPTIONS_WITHOUT_ARG_TABLE
+# undef ENTRY
+}	get_args(int argc, char **argv);
 
 #endif /* !OPTIONS_H_ */
--- src/options.c
+++ src/options.c
@@ -4,43 +4,46 @@
 ** Made by SRJanel
 ** Login SRJanel <n******.******s@epitech.eu>
 ** 
-** Started on  Mon Oct 23 01:26:58 2017 
-** Last update Mon Oct 23 01:36:41 2017 
+** Started on  Sat Oct 14 21:53:05 2017 
+** Last update Mon Dec  4 18:21:51 2017 
 */
 
-#include <stdlib.h>
+#include <unistd.h>
 #include <getopt.h>
 #include "options.h"
 
-struct s_options	options;
-
-static struct option	g_long_options[] =
+struct s_options			get_args(int argc, char **argv)
   {
-    {"interface", required_argument, NULL, 'i'},
-    {"filter", required_argument, NULL, 'f'},
-    {"promiscuous", no_argument, &options.promiscuous, 1},
-    {"help", no_argument, &options.help, 1},
-    {NULL, 0, NULL, 0}
-  };
-
-struct s_options	get_args(int argc, char **argv)
-  {
-    int			c;
-    int			index;
-    struct s_options	options = {.0, NULL, .0, NULL};
+    int					c;
+    int					index;
+    struct s_options			options = {0};
+    static struct option			long_options[] =
+      {
+# define ENTRY(FIELD, REQ, SHORTARG) {MAKE_STRING(FIELD), REQ, NULL, SHORTARG},
+	OPTIONS_WITH_ARG_TABLE
+# undef ENTRY
+# define ENTRY(FIELD, SHORTARG) {MAKE_STRING(FIELD), no_argument, NULL, 1},
+	OPTIONS_WITHOUT_ARG_TABLE
+# undef ENTRY
+	{NULL, 0, NULL, 0}
+      };
 
     while (1)
       {
-	if ((c = getopt_long(argc, argv, "hpi:f:",
-			     g_long_options, &index)) == -1)
+	if ((c = getopt_long(argc, argv,
+			     OPTSTRING,
+			     long_options, &index)) == -1)
 	  break ;
-	else if (c == 'i')
-	  options.interface = optarg;
-	else if (c == 'f')
-	  options.filter = optarg;
-	else if (c == 'p')
-	  options.promiscuous = 1;
-	else if (c == 'h' || c == '?')
+
+	if (optarg && *optarg == '-') continue ;
+# define ENTRY(FIELD, UNUSED1, SHORTARG) else if (c == SHORTARG && !options.FIELD) \
+	  options.FIELD = optarg;
+	OPTIONS_WITH_ARG_TABLE
+#undef ENTRY
+# define ENTRY(FIELD, SHORTARG) else if (c == SHORTARG) options.FIELD = 1;
+	  OPTIONS_WITHOUT_ARG_TABLE
+#undef ENTRY
+	else if (c == '?')
 	  options.help = 1;
       }
     return (options);
